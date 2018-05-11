70d0668d6dfe8470839c1a5829baa69b66c893ef
Dev3211 <devsarma321@gmail.com>
2018-05-11 13:42:32 +0530

    Handle game port through the config file

--- rungame.js
+++ rungame.js
@@ -26,7 +26,7 @@ connection.connect(function(error) {
     console.log('Successful MySQL connection');
 });
 
-Game1 = 6113;
+Game1 = config.Ports.Game;
 
 var Game = {
 
@@ -48,6 +48,88 @@ var Game = {
                 data = data.toString().split('\0')[0];
                 console.log('Incoming data: ' + data + "\r\n")
                 if (data == "<msg t='sys'><body action='verChk' r='0'><ver v='153' /></body></msg>" | data == "<msg t='sys'><body action='rndK' r='-1'></body></msg>" | data.startsWith("<msg t='sys'><body action='login' r='0'>")) {
+                    parseXml(data, function(err, result) {
+                        var type = result.msg['$'].t,
+                            action = result.msg.body[0]['$'].action;
+
+                        var method = Game.xmlHandlers[type][action];
+
+                        if (typeof Game[method] == 'function') {
+                            Game[method](data, client);
+                        }
+                    });
+                }
+                var dataType = data.charAt(0);
+                if (dataType == '%') {
+                    game.handleraw(data, clientObj);
+                }
+            });
+            socket.on('end', function() {
+                game.removeClient(socket);
+                console.log('A client has disconnected');
+            });
+            socket.on('error', function(err) {
+                if (err.code !== 'ECONNRESET') {
+                    console.log(err.toString());
+                }
+            });
+        });
+
+        server.listen(Game1, function() {
+            console.log('Server is listening on port 6113');
+        });
+
+    },
+
+    handleVerChk: function() {
+        clientObj.write('<msg t="sys"><body action="apiOK" r="0"></body></msg>');
+    },
+
+
+    handleRndK: function() {
+        clientObj.set('randomKey', loginkey);
+        clientObj.write('<msg t="sys"><body action="rndK" r="-1"><k>' + loginkey + '</k></body></msg>');
+    },
+
+    handleLogin: function(data) {
+        parseXml(data, function(err, res) {
+            var username = res.msg.body[0].login[0].nick[0],
+                pass = res.msg.body[0].login[0].pword[0];
+            connection.execute("SELECT `ID`, `user` FROM `users` WHERE `user` = ?", [username], function(error, results, fields) {
+                if (results.length === 0) {
+                    clientObj.write('%xt%e%-1%' + USERNAME_NOT_FOUND + '%');
+                } else {
+                    connection.execute("SELECT * FROM `users` WHERE `user` = ?", [username], function(error, result, fields) {
+                        if (result.length != 0) {
+                            var hash = result[0].password.toUpperCase();
+                            var ID = result[0].ID;
+                            var key = result[0].loginkey;
+                            var hash = pass.substr(32);
+                            var key = result[0].loginkey;
+                            var userarray = result[0];
+							
+                            if (hash == key) {
+                                var sql = "UPDATE `users` set `loginkey` = ? WHERE `user` = ?";
+                                var query = connection.execute(sql, [' ', username], function(err, result) {
+                                    console.log("Login key updated!");
+                                });
+                                clientObj.sendXt('l');
+                                clientObj.setClient(userarray);
+                            } else {
+                                clientObj.write('%xt%e%-1%' + INCORRECT_PASSWORD + '%');
+                            }
+                        }
+                    });
+                }
+
+            });
+        })
+    }
+}
+
+Game.handleGame();
+
+module.exports = Game;
                     parseXml(data, function(err, result) {
                         var type = result.msg['$'].t,
                             action = result.msg.body[0]['$'].action;
