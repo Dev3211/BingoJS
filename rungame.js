8dfac5d99ba638d62014fc923c699181e9cf9c2e
Dev3211 <devsarma321@gmail.com>
2018-05-13 17:49:37 +0530

    Update
    Query updates for igloo

--- rungame.js
+++ rungame.js
@@ -7,7 +7,7 @@ var game = require('./handlers/handleGame'),
     db = require('mysql2'),
     config = require("./config"),
     md5 = require('md5'),
-	log = require('js-logs'),
+    log = require('js-logs'),
     error = require('./handlers/Errors.js');
 
 var connection = db.createConnection({
@@ -100,107 +100,35 @@ var Game = {
                     clientObj.write('%xt%e%-1%' + USERNAME_NOT_FOUND + '%');
                 } else {
                     connection.execute("SELECT * FROM `users` WHERE `user` = ?", [username], function(error, result, fields) {
-                        if (result.length != 0) {
-                            var hash = result[0].password.toUpperCase();
-                            var ID = result[0].ID;
-                            var key = result[0].loginkey;
-                            var hash = pass.substr(32);
-                            var key = result[0].loginkey;
-                            var userarray = result[0];
-							
-                            if (hash == key) {
-                                var sql = "UPDATE `users` set `loginkey` = ? WHERE `user` = ?";
-                                var query = connection.execute(sql, [' ', username], function(err, result) {
-                                    console.log("Login key updated!");
-                                });
-                                clientObj.sendXt('l');
-                                clientObj.setClient(userarray);
-                            } else {
-                                clientObj.write('%xt%e%-1%' + INCORRECT_PASSWORD + '%');
-                            }
-                        }
-                    });
-                }
-
-            });
-        })
-    }
-}
-
-Game.handleGame();
-
-module.exports = Game;
-                    parseXml(data, function(err, result) {
-                        var type = result.msg['$'].t,
-                            action = result.msg.body[0]['$'].action;
-
-                        var method = Game.xmlHandlers[type][action];
-
-                        if (typeof Game[method] == 'function') {
-                            Game[method](data, client);
-                        }
-                    });
-                }
-                var dataType = data.charAt(0);
-                if (dataType == '%') {
-                    game.handleraw(data, clientObj);
-                }
-            });
-            socket.on('end', function() {
-                game.removeClient(socket);
-                console.log('A client has disconnected');
-            });
-            socket.on('error', function(err) {
-                if (err.code !== 'ECONNRESET') {
-                    console.log(err.toString());
-                }
-            });
-        });
-
-        server.listen(Game1, function() {
-            console.log('Server is listening on port 6113');
-        });
-
-    },
-
-    handleVerChk: function() {
-        clientObj.write('<msg t="sys"><body action="apiOK" r="0"></body></msg>');
-    },
-
-
-    handleRndK: function() {
-        clientObj.set('randomKey', loginkey);
-        clientObj.write('<msg t="sys"><body action="rndK" r="-1"><k>' + loginkey + '</k></body></msg>');
-    },
-
-    handleLogin: function(data) {
-        parseXml(data, function(err, res) {
-            var username = res.msg.body[0].login[0].nick[0],
-                pass = res.msg.body[0].login[0].pword[0];
-            connection.execute("SELECT `ID`, `user` FROM `users` WHERE `user` = ?", [username], function(error, results, fields) {
-                if (results.length === 0) {
-                    clientObj.write('%xt%e%-1%' + USERNAME_NOT_FOUND + '%');
-                } else {
-                    connection.execute("SELECT * FROM `users` WHERE `user` = ?", [username], function(error, result, fields) {
-                        if (result.length != 0) {
-                            var hash = result[0].password.toUpperCase();
-                            var ID = result[0].ID;
-                            var key = result[0].loginkey;
-                            var hash = pass.substr(32);
-                            var key = result[0].loginkey;
-                            var userarray = result[0];
-							
-                            if (hash == key) {
-                                var sql = "UPDATE `users` set `loginkey` = ? WHERE `user` = ?";
-                                var query = connection.execute(sql, [' ', username], function(err, result) {
-                                    console.log("Login key updated!");
-                                });
-                                clientObj.sendXt('l');
-                                clientObj.setClient(userarray);
-                            } else {
-                                clientObj.write('%xt%e%-1%' + INCORRECT_PASSWORD + '%');
-                            }
-                        }
+                        connection.execute("SELECT * FROM `furnitures` WHERE `username` = ?", [username], function(error, result1, fields) {
+                            connection.execute("SELECT * FROM `items` WHERE `username` = ?", [username], function(error, result2, fields) {
+                                if (result.length != 0) {
+                                    var hash = result[0].password.toUpperCase();
+                                    var ID = result[0].ID;
+                                    var key = result[0].loginkey;
+                                    var hash = pass.substr(32);
+                                    var key = result[0].loginkey;
+                                    var userarray = result[0];
+                                    var userarray1 = result1[0];
+                                    var userarray2 = result2[0];
+                                    if (userarray2 == undefined) {
+                                        userarray2 = [];
+                                    } else if (userarray1 == undefined) {
+                                        userarray1 = [];
+                                    }
+                                    if (hash == key) {
+                                        var sql = "UPDATE `users` set `loginkey` = ? WHERE `user` = ?";
+                                        var query = connection.execute(sql, [' ', username], function(err, result) {
+                                            console.log("Login key updated!");
+                                        });
+                                        clientObj.sendXt('l');
+                                        clientObj.setClient(userarray, userarray1, userarray2);
+                                    } else {
+                                        clientObj.write('%xt%e%-1%' + INCORRECT_PASSWORD + '%');
+                                    }
+                                }
+                            });
+                        });
                     });
                 }
 
